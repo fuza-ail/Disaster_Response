b718ba8937c87b312aa92ad94bf2033cbbb25d1c
Muhammad Fuzail <mfuzail1991@gmail.com>
2021-12-05 16:34:00 +0700

    machine learning pipeline

--- models/train_classifier.py
+++ models/train_classifier.py
@@ -1,24 +1,68 @@
 import sys
+import numpy as np
+import pandas as pd
 
+import sqlite3
+from sqlalchemy import create_engine
+
+import nltk
+nltk.download('wordnet')
+from nltk.tokenize import word_tokenize
+from nltk.stem import WordNetLemmatizer
+
+from sklearn.model_selection import train_test_split
+from sklearn.pipeline import Pipeline
+from sklearn.multioutput import MultiOutputClassifier
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.naive_bayes import GaussianNB
+from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer
+from sklearn.base import TransformerMixin
+from sklearn.metrics import classification_report
+from sklearn.model_selection import GridSearchCV
+
+import joblib
 
 def load_data(database_filepath):
-    pass
-
+    conn = sqlite3.connect(database_filepath)
+    engine = create_engine('sqlite:///{}'.format(database_filepath))
+    df = pd.read_sql('select * from disaster',conn)
+    
+    X = df['message']
+    Y = df.loc[:,'related':]
+    
+    return X.values, Y.values, Y.columns
 
 def tokenize(text):
-    pass
+    tokens = word_tokenize(text)
+    lemmatizer = WordNetLemmatizer()
+    
+    clean_token = []
+    for token in tokens:
+        clean_tok = lemmatizer.lemmatize(token).lower().strip()
+        clean_token.append(clean_tok)
+    
+    return clean_token 
 
 
 def build_model():
-    pass
+    pipeline = Pipeline([
+        ('vect', CountVectorizer(tokenizer=tokenize)),
+        ('tfid',TfidfTransformer()),
+        ('rf', MultiOutputClassifier(RandomForestClassifier()))
+    ])
+    
+    return pipeline
 
 
 def evaluate_model(model, X_test, Y_test, category_names):
-    pass
+    Y_pred = model.predict(X_test)
 
+    for i,col in enumerate(category_names):
+        print(col)
+        print(classification_report(Y_test[i],Y_pred[i]))
 
 def save_model(model, model_filepath):
-    pass
+    joblib.dump(model, model_filepath)
 
 
 def main():
